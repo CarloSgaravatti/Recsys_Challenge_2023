d84a4f067b4187ef36d2cc26c0dcc9b04c9b48e2
Carlo Sgaravatti <carlo.sgaravatti@gmail.com>
2023-11-30 20:28:31 +0100

    add top pop to linear combination

--- Recommenders/Hybrid/LinearCombinationRecommender.py
+++ Recommenders/Hybrid/LinearCombinationRecommender.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.sparse as sps
 
 from Recommenders.BaseRecommender import BaseRecommender
 
@@ -8,6 +9,11 @@ class LinearCombinationRecommender(BaseRecommender):
         super(LinearCombinationRecommender, self).__init__(URM_train)
         self.recommenders = recommenders
         self.weights = weights
+
+    def _calculate_top_pop_items(self, cutoff):
+        item_popularity = np.ediff1d(sps.csc_matrix(self.URM_train).indptr)
+        self.filterTopPop = True
+        self.filterTopPop_ItemsID = item_popularity.argsort()[-cutoff:]
         
     def _compute_item_score(self, user_id_array, items_to_compute=None):
         scores = np.zeros((len(user_id_array), self.n_items))
